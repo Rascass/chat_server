5ad519b869f64ca025ea779113f945650eb2cf45
Vladislav Cehanovich <vlad03122000@gmail.com>
2020-12-17 18:30:40 +0300

    fixed writing to the database

--- src/main/java/main/Server.java
+++ src/main/java/main/Server.java
@@ -3,15 +3,16 @@ package main;
 
 
 
-import model.Client;
-import model.Connection;
-import model.ConnectionListener;
-import model.Phone;
+import model.*;
 import service.ClientService;
+import service.SessionService;
 
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.time.OffsetDateTime;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 public class Server {
 
@@ -26,15 +27,28 @@ public class Server {
                     String request = phone.readLine();
                     System.out.println("Request: " + request);
                     String response = "";
+
                     Client currentClient;
+
                     ClientService clientService = new ClientService();
+                    SessionService sessionService = new SessionService();
+
                     boolean notFind = true;
                     int token = 1 + (int) (Math.random() * 100000);
+                    List<Client> clients = new ArrayList<>();
+                    Session session = new Session(new Date(), 8000, true, "127.0.0.1",
+                            "127.0.0.1", token, clients);
+
 
                     for (Client c: clientService.getAllClients()) {
                         if (request.contains(c.getLogin()) && request.contains(c.getPassword() + "")) {
                             currentClient = c;
                             currentClient.setClientToken(token);
+                            clients.add(currentClient);
+                            token = 1 + (int) (Math.random() * 100000);
+
+                            sessionService.createSession(session);
+                            clientService.updateClient(currentClient);
                             notFind = false;
                             break;
                         }
@@ -49,7 +63,10 @@ public class Server {
                         to = request.lastIndexOf("}");
                         String password = request.substring(from, to);
 
-                        clientService.createClient(new Client(token, login, password, null));
+                        currentClient = new Client(token, login, password, null);
+                        clients.add(currentClient);
+                        clientService.createClient(currentClient);
+                        sessionService.createSession(session);
                     }
                     phone.writeLine(response);
                 }).start();
